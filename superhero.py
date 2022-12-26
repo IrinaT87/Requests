69352069e5f2b554694286aaacc9b85050df21b7
IrinaT87 <118206816+IrinaT87@users.noreply.github.com>
2022-12-27 00:12:23 +0300

    Update superhero.py

--- superhero.py
+++ superhero.py
@@ -2,17 +2,16 @@ import requests
 import json
 
 intelligence_dict={}
-def get_heroes_info(name):
+def get_heroes_info(*name):
     URI='https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api'
     request='/all.json'
-    response=list(filter(lambda hero: hero['name']==name, requests.get(url=URI+request).json()))
-    for el in response:
-        intelligence=el['powerstats']['intelligence']
-        intelligence_dict[name]=intelligence
+    for n in name:
+        response=list(filter(lambda hero: hero['name']==n, requests.get(url=URI+request).json()))
+        for el in response:
+            intelligence=el['powerstats']['intelligence']
+            intelligence_dict[n]=intelligence
     
-get_heroes_info('Thanos')
-get_heroes_info('Hulk')
-get_heroes_info('Captain America')
+get_heroes_info(*['Thanos','Hulk','Captain America'])
 
 print(intelligence_dict)
 max_intelligence = max(intelligence_dict, key=intelligence_dict.get)
